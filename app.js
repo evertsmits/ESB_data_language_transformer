d3968b06d094113e23ec9ba9668c06bd8ce35c02
evertsmits <egsdevelopmentmail@gmail.com>
2018-03-04 14:48:19 +0100

    Able to send data

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const app = express();
+const app = express(), bodyParser = require('body-parser') ;
+require('body-parser-xml')(bodyParser);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+app.use(bodyParser.xml());
+
+app.get('/', (req, res) => res.send('Welcome to the ESB. Send JSON data to localhost:3000/JSON in order to retrieve XML, and send XML data to localhost:3000/XML to retireve JSON'));
+
+app.post('/JSON', (req, res) => console.log(req.body));
+
+app.post('/XML', (req, res) => console.log(req.body));
 
 app.listen(3000, () => console.log('Example app is listening on port 3000'));
